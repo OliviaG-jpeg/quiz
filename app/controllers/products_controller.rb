503469912113f9e379716bbdfc279a25f86c5a2b
Olivia Goehrig <goehrigo@gmail.com>
2019-11-09 20:50:49 +0000

    help

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    @product = Product.all
+    @products = Product.all
+  end
+  def new
+    @product = Product.new
+  end
+  def create
+    Product.create(product_params)
+    redirect_to root_path
+  end
+  private
+  def product_params
+    params.require(:product).permit(:name, :brand, :description)
   end
 end
